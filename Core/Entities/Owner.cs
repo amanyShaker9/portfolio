2cadba976ad39240c0e7e04740d2c121cfc6feb9
amanyShaker9 <58103474+amanyShaker9@users.noreply.github.com>
2022-06-26 00:04:29 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Owner:EntityBase
+    {
+        public string FullName { get; set; }
+        public string Profile { get; set; }
+        public string Image { get; set; }
+        public Address Address { get; set; }
+    }
+
+
+}
+   
---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectName{ get; set; }
+        public string Description { get; set; }
+        public string ProjectImage { get; set; }
+    }
+
+
+}
+   
